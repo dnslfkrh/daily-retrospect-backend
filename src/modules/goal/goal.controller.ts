03a6e073765307a2053fb6c587b8a13be7169b92
yongwuk <lyw3059@gmail.com>
2025-03-17 15:31:14 +0900

    feat: goal create, update, delete

--- src/modules/goal/goal.controller.ts
+++ src/modules/goal/goal.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { GoalService } from "./goal.service";
 import { UserSub } from "src/common/types/Payload";
 import { User } from "src/common/decorators/user.decorator";
+import { CreateGoalDto, UpdateGoalDto } from "./\bdto/goal.dto";
 
 @Controller('goal')
 export class GoalController {
@@ -17,5 +18,20 @@ export class GoalController {
   @Get('finished')
   async getFinishedGoals(@User() user: UserSub) {
     return await this.goalService.getFinishedGoals(user);
+  }
+
+  @Post("create")
+  async createGoal(@User() user: UserSub, @Body() createGoalDto: CreateGoalDto) {
+    return this.goalService.createGoal(user, createGoalDto);
+  }
+
+  @Put("update")
+  async updateGoal(@User() user: UserSub, @Body() updateGoalDto: UpdateGoalDto) {
+    return this.goalService.updateGoal(user, updateGoalDto);
+  }
+
+  @Delete(":id")
+  async deleteGoal(@User() user: UserSub, @Param("id") id: string) {
+    return this.goalService.deleteGoal(user, parseInt(id, 10));
   }
 }
