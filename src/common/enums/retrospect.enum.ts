a292d03975d3ac5faa09e22692b8fbbdcad4eb49
yonguk <lyw3059@gmail.com>
2025-03-20 15:27:54 +0900

    fix: retrospect entity

--- src/common/enums/retrospect.enum.ts
+++ src/common/enums/retrospect.enum.ts
@@ -1,4 +1,5 @@
 export enum RetrospectConcept {
+  COMMON = 'common', // 공통
   EMOTION = 'emotion', // 감정 중심
   EVENT = 'event', // 사건 중심
   REFLECTION = 'reflection', // 성찰 중심
@@ -11,7 +12,8 @@ export enum RetrospectVolume {
 }
 
 export enum AnswerType {
-  TEXT = 'text',
-  SINGLE_CHOICE = 'single_choice',
-  MULTI_CHOICE = 'multi_choice',
+  TEXT = 'text',  // 자유 입력형 (문장)
+  SINGLE_CHOICE = 'single_choice',  // 단일 선택형 (라디오 버튼, 이모티콘 등)
+  MULTI_CHOICE = 'multi_choice',  // 다중 선택형 (배열, 키워드 여러 개 선택)
+  SCORE = 'score'  // 점수형 (1~10점 스크롤 입력)
 }
--- src/modules/retrospect/entity/answer.entity.ts
+++ src/modules/retrospect/entity/answer.entity.ts
@@ -1,5 +1,4 @@
 import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn } from 'typeorm';
-import { RetrospectSetting } from './setting.entity';
 import { RetrospectQuestion } from './question.entity';
 import { AnswerType } from 'src/common/enums/retrospect.enum';
 import { Goal } from 'src/modules/goal/entitiy/goal.entity';
@@ -9,14 +8,11 @@ export class RetrospectAnswer {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ManyToOne(() => RetrospectSetting, (setting) => setting.answers, { onDelete: 'CASCADE' })
-  retrospectSetting: RetrospectSetting;
-
   @ManyToOne(() => RetrospectQuestion, { nullable: true })
   question: RetrospectQuestion;
 
   @Column({ type: 'enum', enum: AnswerType })
-  answer_type: AnswerType; // text, single_choice, multi_choice
+  answer_type: AnswerType;
 
   @Column({ type: 'text', nullable: true })
   answer?: string; // 자유 입력형 답변
@@ -26,6 +22,9 @@ export class RetrospectAnswer {
 
   @Column({ type: 'simple-array', nullable: true })
   selected_choices?: string[]; // 다중 선택형 답변
+
+  @Column({ type: 'int', nullable: true })
+  score?: number; // 점수형 답변 (1~10)
 
   @ManyToOne(() => Goal, { nullable: true, onDelete: 'SET NULL' })
   goal?: Goal; // 특정 목표와 연관된 회고 답변
