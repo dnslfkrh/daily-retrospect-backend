f18f938a43d1eb1f5d81feeba66813dc79885ba3
yonguk <lyw3059@gmail.com>
2025-04-02 14:25:06 +0900

    fix: remove answer-answer type

--- src/modules/cron/cron.scheduler.ts
+++ src/modules/cron/cron.scheduler.ts
@@ -14,27 +14,48 @@ export class CronScheduler {
   ) { }
 
   /* 리마인드 이메일 */
-  @Cron('55 12 * * *')
+  // @Cron('55 12 * * *')
   async sendReminderEmails() {
     await this.userService.sendRemindersToInactiveUsers(2);
   }
 
   /* 회고 AI 요약 */
-  @Cron('13 2 * * *')
-  async analyzeRetrospects() {
+  // @Cron('13 2 * * *')
+  async analyzeRetrospects1() {
+    console.log('AI 요약 시작');
+
     const retrospect = await this.retrospectService.getYesterdayAnswers();
 
     const promises = retrospect.map(async (userRetrospect: UserRetrospectProps) => {
       const sessionId = userRetrospect.sessionId;
       const userId = userRetrospect.userId;
       const retrospectSummary = await this.aiService.summarizeRetrospect(userRetrospect.answers);
-      console.log(`AI 요약: ${userId} - ${sessionId} - ${retrospectSummary}`);
+
+      console.log(`AI 요약 완료: ${userId} - ${sessionId} - ${retrospectSummary}`);
 
       await this.retrospectService.saveSummary(sessionId, userId, retrospectSummary);
     });
 
-    console.log('AI 요약 시작');
     await Promise.all(promises);
+
     console.log('AI 요약 완료');
   }
+
+  // async analyzeRetrospects2() {
+  //   console.log('AI 요약 시작');
+
+  //   const retrospect = await this.retrospectService.getYesterdayAnswers();
+
+  //   for (const userRetrospect of retrospect) {
+  //     const sessionId = userRetrospect.sessionId;
+  //     const userId = userRetrospect.userId;
+  //     const retrospectSummary = await this.aiService.summarizeRetrospect(userRetrospect.answers);
+
+  //     console.log(`AI 요약 완료: ${userId} - ${sessionId} - ${retrospectSummary}`);
+
+  //     await this.retrospectService.saveSummary(sessionId, userId, retrospectSummary);
+  //   }
+
+  //   console.log('AI 요약 완료');
+  // }
 }
