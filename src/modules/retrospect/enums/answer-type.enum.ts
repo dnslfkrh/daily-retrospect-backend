0e04e134b3d558edf675bf259168ea549ec85a08
yonguk <lyw3059@gmail.com>
2025-05-31 00:51:16 +0900

    feat: add time answer

--- src/modules/retrospect/enums/answer-type.enum.ts
+++ src/modules/retrospect/enums/answer-type.enum.ts
@@ -2,5 +2,6 @@ export enum RetrospectAnswerTypeEnum {
   TEXT = 'text',
   SINGLE_CHOICE = 'single_choice',
   MULTI_CHOICE = 'multi_choice',
-  SCORE = 'score'
+  SCORE = 'score',
+  TIME = 'time',
 }
